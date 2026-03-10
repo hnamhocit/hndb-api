c27496dc21e7ff72886202894e2a7ab58a262e23
hnamhocit <hnamhocit@gmail.com>
2026-03-10 23:31:47 +0700

    update

--- src/middlewares/setup.middleware.ts
+++ src/middlewares/setup.middleware.ts
@@ -4,6 +4,15 @@ import helmet from 'helmet'
 
 export const setupMiddlewares = (app: Express) => {
 	app.use(helmet({ contentSecurityPolicy: false }))
-	app.use(cors())
+	app.use(
+		cors({
+			origin: [
+				'https://hndb.space',
+				'https://www.hndb.space',
+				'http://localhost:3000',
+			],
+			credentials: true,
+		}),
+	)
 	app.use(express.json({ limit: '1mb' }))
 }
